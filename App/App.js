963526a907c045de92fc57eb79090cf70d6e626d
victor houze <victor.houze@epitech.eu>
2021-04-20 22:11:11 +0200

    {+} Navigation done{+}

--- App/App.js
+++ App/App.js
@@ -3,18 +3,51 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './components/HomeScreen';
 import ProfileScreen from './components/ProfileScreen';
-import MapsScreen from './components/MapsScreen';
+// import MapsScreen from './components/MapsScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text, View } from 'react-native';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// Fonction à supprimer Pour tester la fonction de la map de Maxime et décommenter l'import (j'ai des problèmes de tests avec mon pc)
+function MapsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Maps Screen</Text>
+    </View>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Page d'accueil"}}/>
-        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Profil"}}/>
-        <Stack.Screen name="Maps" component={MapsScreen} options={{ title: "Maps"}}/>        
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person-circle-outline' : 'person-outline';
+            } else if (route.name === 'Maps') {
+              iconName = focused ? 'map-outline' : 'navigate-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Maps" component={MapsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- App/components/ProfileScreen.js
+++ App/components/ProfileScreen.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export default function ProfileScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text> Profile Screen </Text>
       </View>
     );
