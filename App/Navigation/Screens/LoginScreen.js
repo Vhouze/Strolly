64a9aec00a5a4521c2960d431d2929d04e894621
victor houze <victor.houze@epitech.eu>
2021-04-27 00:39:07 +0200

    {+} Adding the Login Page and the swipe page {+}

--- App/Navigation/Screens/LoginScreen.js
+++ App/Navigation/Screens/LoginScreen.js
@@ -1,14 +1,103 @@
-import { View, Text, Button } from 'react-native';
-import * as React from 'react';
+import { View, Text, Button, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+
+function check_id(email, password) {
+  console.log(email)
+  console.log(password)  
+  return;
+}
 
 export default function LoginScreen({navigation}) {
+  const [email, setText] = useState('Guest');
+  const [password, setPass] = useState('');  
+  const move = () => {
+    check_id(email, password);
+    navigation.navigate('Mood Screen')
+  };
+  return (
+    <View style={styles.container}>
+    <Text style={styles.logo}>Strolly</Text>
+    <View style={styles.inputView} >
+      <TextInput  
+        style={styles.inputText}
+        placeholder="Email..." 
+        placeholderTextColor="#003f5c"
+        onChangeText={email => setText(email)}
+        // value={state.email}
+      />
+    </View>
+    <View style={styles.inputView} >
+      <TextInput  
+        secureTextEntry
+        style={styles.inputText}
+        placeholder="Password..." 
+        placeholderTextColor="#003f5c"
+        onChangeText={password => setPass(password)}
+        />
+    </View>
+    <TouchableOpacity>
+      <Text style={styles.forgot}>Forgot Password?</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={move} style={styles.loginBtn}>
+      <Text style={styles.loginText}>LOGIN</Text>
+    </TouchableOpacity>
+    <TouchableOpacity>
+      <Text style={styles.loginText}>Signup</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={move}>
+      <Text style={styles.loginTextBis}>Continue as Guest</Text>
+    </TouchableOpacity>
+    </View>
+  );
+}
 
-    const move = () => { navigation.navigate('Mood Screen')};
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Login Screen</Text>
-        <Button onPress={move} title="next"/>
-      </View>
-    );
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  },
+  loginTextBis:{
+    margin: "15px",
+    color:"grey"
   }
+});
--- App/Navigation/Screens/SwipeScreen.js
+++ App/Navigation/Screens/SwipeScreen.js
@@ -1,14 +1,122 @@
-import { View, Text, Button } from 'react-native';
-import * as React from 'react';
+import React, { useState } from 'react'
+import styled from 'styled-components/native'
+import TinderCard from 'react-tinder-card'
+// import Activités from '../Db/Activités.json'
+
+const Container = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
+
+const Header = styled.Text`
+    color: #000;
+    font-size: 30px;
+    margin-bottom: 30px;
+`
+
+const CardContainer = styled.View`
+    width: 90%;
+    max-width: 260px;
+    height: 300px;
+`
+
+const Card = styled.View`
+    position: absolute;
+    background-color: #fff;
+    width: 100%;
+    max-width: 260px;
+    height: 300px;
+    shadow-color: black;
+    shadow-opacity: 0.2;
+    shadow-radius: 20px;
+    border-radius: 20px;
+    resize-mode: cover;
+`
+
+const CardImage = styled.ImageBackground`
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 20px;
+`
+
+const CardTitle = styled.Text`
+    position: absolute;
+    bottom: 0;
+    margin: 10px;
+    color: #fff;
+`
+
+const InfoText = styled.Text`
+    height: 28px;
+    justify-content: center;
+    display: flex;
+    z-index: -100;
+`
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    img: require('../../assets/img/Swipe/1.jpeg')
+  },
+  {
+    name: 'Erlich Bachman',
+    img: require('../../assets/img/Swipe/2.jpeg')
+  },
+  {
+    name: 'Monica Hall',
+    img: require('../../assets/img/Swipe/3.png')
+  },
+  {
+    name: 'Jared Dunn',
+    img: require('../../assets/img/Swipe/1.jpeg')
+  },
+  {
+    name: 'Dinesh Chugtai',
+    img: require('../../assets/img/Swipe/2.jpeg')
+  }
+]
 
 export default function SwipeScreen({navigation,route}) {
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+    if (direction === 'right') {
+      console.log('right');
+      move();
+    }
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
 
   const move = () => {navigation.navigate('Maps Screen')};
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Swipe Screen</Text>
-        <Button onPress={move} title="next"/>
-      </View>
-    );
-  }
+
+  return (
+    <Container>
+      <Header>Where do you want to go ?</Header>
+      <CardContainer>
+        {characters.map((character) =>
+          <TinderCard key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+          {/* <TinderCard key={character.Name} onSwipe={(dir) => swiped(dir, character.Name)} onCardLeftScreen={() => outOfFrame(character.Name)}> */}
+          <Card>
+            <CardImage source={character.img}>
+            {/* <CardImage source={character.Photo}> */}
+            {/* <CardTitle>{character.Name}</CardTitle> */}
+            <CardTitle>{character.name}</CardTitle>
+              </CardImage>
+            </Card>
+          </TinderCard>
+        )}
+      </CardContainer>
+      {lastDirection ? <InfoText>You swiped {lastDirection}</InfoText> : <InfoText />}
+    </Container>
+  )
+}
