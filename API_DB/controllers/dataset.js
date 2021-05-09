2969bdb9cfae274a9170d60e011fb5b5f7eda4b1
Thomas Lopvet <thomas.lopvet@epitech.eu>
2021-05-09 18:57:29 +0200

    [back - dataset] fix mood output

--- API_DB/controllers/dataset.js
+++ API_DB/controllers/dataset.js
@@ -5,10 +5,17 @@ module.exports = {
 	GetCafe: function(app, req, res) {
 		let link = "./PhantomBuster_dataset/Clean_DB_final.csv"
 	    let data = {code: "", message: {}};
+	    let i = 0;
+
 		csv()
 		.fromFile(link)
 		.then((jsonObj) => {
     		data.code = "SUCCESS"
+    		while (i < Object.keys(jsonObj).length) {
+    			if (jsonObj[i].mood != "")
+    				jsonObj[i].mood = jsonObj[i].mood.split("'")[1];
+    			i++;
+    		}
             data.message = jsonObj;
             res.status(200).send(data)
         	return;
