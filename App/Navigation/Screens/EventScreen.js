bc0e4f7ba6b37e0dee57016af6e384709f89fb95
Caille Maxence <caille.maxence@outlook.fr>
2021-06-16 12:52:11 +0200

    new event screen

--- App/Navigation/Screens/EventScreen.js
+++ App/Navigation/Screens/EventScreen.js
@@ -2,11 +2,24 @@ import { View, Text, Button,StyleSheet, FlatList, TouchableOpacity, Platform, St
 import React from 'react';
 import Color from '../../Constant/Color';
 import MoodList from '../../Components/Mood/MoodList';
+import LottieView from 'lottie-react-native';
+
 
 export default function EventScreen({navigation}) {
+
+    const notif = = () => {
+      };
+
     return (
-        <View style={{alignItems:"center"}}>
-            <Text style={{justifyContent: "center"}}>Event Screen</Text>
+        <View style={{alignItems:"center", flex:1, backgroundColor: Color.second}}>
+            <Text>Trend|by Club </Text>
+            <Text>Devient un membre de la communauté Trend|by et profite de nombreux avantages ! </Text>
+             <LottieView source={require("../../assets/img/emoji.json")}  autoPlay />
+            <LottieView source={require("../../assets/img/confet.json")} loop ={false} autoPlay />
+            <Text>Le club arrive bientôt à Lyon, encore un peu de patience...</Text>
+            <TouchableOpacity>
+                <Text> Recevoir une notification</Text>
+            </TouchableOpacity>
         </View>
     )
 }
